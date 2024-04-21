bdce5d7eeb283aef2cee293f2827a47c81e9c80a
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2024-04-21 10:04:13 -0600

    Actualizando keys

--- keys.js
+++ keys.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
+        host: 'ls-756ee540411a831fcf3c0a549669fac2303516e3.cnki6c22u7mq.us-east-2.rds.amazonaws.com',
+        user: 'dbmasteruser',
+        password: '9j;6X%4]dgtsQr1[Cv=SMo%FSBZjcHv3',
+        port: 3306,
         database: 'f1'
     }
 };
